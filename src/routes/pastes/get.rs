bb9a5613d0f9d83f7a27564e02f4384b2266d031
Kyle Clemens <git@kyleclemens.com>
2018-04-13 19:35:43 -0400

    feat: add support for ?full

--- src/routes/pastes/get.rs
+++ src/routes/pastes/get.rs
@@ -1,18 +1,37 @@
 use errors::*;
-use models::paste::{Paste, PasteFile, Content, PasteId};
+use models::paste::{Paste, Content, PasteId};
+use models::paste::output::{Output, OutputFile};
 
 use rocket_contrib::Json;
 
 use std::fs::File;
 use std::io::Read;
 
+#[derive(Debug, Default, FromForm)]
+struct Query {
+  full: Option<bool>,
+}
+
+// routes separated because of https://github.com/SergioBenitez/Rocket/issues/376
+
 #[get("/<id>")]
-fn get(id: PasteId) -> Result<Json<Paste>> {
+fn get(id: PasteId) -> Result<Json<Output>> {
+  _get(id, None)
+}
+
+#[get("/<id>?<query>")]
+fn get_query(id: PasteId, query: Query) -> Result<Json<Output>> {
+  _get(id, Some(query))
+}
+
+fn _get(id: PasteId, query: Option<Query>) -> Result<Json<Output>> {
   let files_dir = id.files_directory();
   // FIXME: check if dir exists and return 404 instead of failing below and returning 500
 
   let metadata = id.metadata()?;
   let internal = id.internal()?;
+
+  let query = query.unwrap_or_default();
 
   let mut files = Vec::with_capacity(internal.names.len());
   for (uuid, name) in &*internal.names {
@@ -22,19 +41,26 @@ fn get(id: PasteId) -> Result<Json<Paste>> {
     file.read_to_end(&mut data)?;
 
     // TODO: store if the file is text or binary instead of attempting to parse
-    let content = String::from_utf8(data.clone())
-      .map(Content::Text)
-      .unwrap_or_else(|_| Content::Base64(data));
+    let content = match query.full {
+      Some(true) => Some(String::from_utf8(data.clone())
+        .map(Content::Text)
+        .unwrap_or_else(|_| Content::Base64(data))),
+      _ => None
+    };
 
-    let pf = PasteFile {
+    let pf = OutputFile {
+      id: uuid.simple().to_string(),
       name: Some(name.clone()),
       content,
     };
     files.push(pf);
   }
 
-  Ok(Json(Paste {
-    metadata,
+  Ok(Json(Output {
+    paste: Paste {
+      metadata,
+      files: Vec::new(),
+    },
     files,
   }))
 }

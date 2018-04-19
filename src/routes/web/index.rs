e8af25c391b7ebb4380e24c8ef2c4573f3f1e3f7
Anna <git@anna.lgbt>
2018-04-19 15:03:48 -0400

    refactor: don't use empty HashMap

--- src/routes/web/index.rs
+++ src/routes/web/index.rs
@@ -1,10 +1,6 @@
-use std::collections::HashMap;
-
 use rocket_contrib::Template;
 
 #[get("/")]
 fn get() -> Template {
-  let mut map: HashMap<String, String> = HashMap::default();
-  map.insert("site_name".into(), "paste.gg".into());
-  Template::render("index", map)
+  Template::render("index", json!({}))
 }

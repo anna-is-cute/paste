c115a8b44b56a87e468a26baa54b70bd18c83bf0
Anna <git@anna.lgbt>
2018-04-22 17:36:17 -0400

    refactor: use alternate stylesheets

--- web/static/js/theme.js
+++
@@ -1,5 +0,0 @@
-if (localStorage.getItem('dark') === null) {
-  document.getElementById('style-dark').rel = 'stylesheet alternate';
-} else {
-  document.getElementById('style-light').rel = 'stylesheet alternate';
-}

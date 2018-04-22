ca95628ed49da42044894de236fc3af6ecdc2cde
Kyle Clemens <git@kyleclemens.com>
2018-04-22 15:09:39 -0400

    feat: implement basic paste creation via web ui

--- src/routes/web/index.rs
+++ src/routes/web/index.rs
@@ -1,15 +1,18 @@
 use config::Config;
 use routes::web::OptionalWebUser;
 
+use rocket::http::{Cookies, Cookie};
 use rocket::State;
 
 use rocket_contrib::Template;
 
 #[get("/")]
-fn get(config: State<Config>, user: OptionalWebUser) -> Template {
+fn get(config: State<Config>, user: OptionalWebUser, mut cookies: Cookies) -> Template {
   let ctx = json!({
     "config": &*config,
     "user": &*user,
+    "error": cookies.get("error").map(|x| x.value()),
   });
+  cookies.remove(Cookie::named("error"));
   Template::render("index", ctx)
 }
--- src/routes/web/pastes/mod.rs
+++ src/routes/web/pastes/mod.rs
@@ -1,1 +1,2 @@
 pub mod get;
+pub mod post;

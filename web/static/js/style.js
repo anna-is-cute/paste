2c68b32dd216a31fc29a545dee446815a1489c88
Anna <git@anna.lgbt>
2018-04-29 18:15:58 -0400

    style: use single quotes

--- web/static/js/style.js
+++ web/static/js/style.js
@@ -1,18 +1,18 @@
 function setActiveStyleSheet(title) {
-  for (var a of document.getElementsByTagName("link")) {
-    if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+  for (var a of document.getElementsByTagName('link')) {
+    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title')) {
       a.disabled = true;
-      if (a.getAttribute("title") === title) {
+      if (a.getAttribute('title') === title) {
         a.disabled = false;
       }
     }
   }
   if (typeof paste_editors === 'object') {
     var theme;
-    if (title === "dark") {
-      theme = "ace/theme/idle_fingers";
-    } else if (title === "light") {
-      theme = "ace/theme/tomorrow";
+    if (title === 'dark') {
+      theme = 'ace/theme/idle_fingers';
+    } else if (title === 'light') {
+      theme = 'ace/theme/tomorrow';
     }
 
     for (var editor of Object.values(paste_editors)) {
@@ -22,18 +22,18 @@ function setActiveStyleSheet(title) {
 }
 
 function getActiveStyleSheet() {
-  for (var a of document.getElementsByTagName("link")) {
-    if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) {
-      return a.getAttribute("title");
+  for (var a of document.getElementsByTagName('link')) {
+    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title') && !a.disabled) {
+      return a.getAttribute('title');
     }
   }
   return null;
 }
 
 function getPreferredStyleSheet() {
-  for (var a of document.getElementsByTagName("link")) {
-    if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("rel").indexOf("alt") == -1 && a.getAttribute("title")) {
-      return a.getAttribute("title");
+  for (var a of document.getElementsByTagName('link')) {
+    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('rel').indexOf('alt') == -1 && a.getAttribute('title')) {
+      return a.getAttribute('title');
     }
   }
   return null;
@@ -41,27 +41,27 @@ function getPreferredStyleSheet() {
 
 function swapTheme() {
   var next;
-  if (getActiveStyleSheet() === "dark") {
-    next = "light";
+  if (getActiveStyleSheet() === 'dark') {
+    next = 'light';
   } else {
-    next = "dark";
+    next = 'dark';
   }
   setActiveStyleSheet(next);
 }
 
 (function() {
   window.onload = function(e) {
-    var style = this.localStorage.getItem("style");
+    var style = this.localStorage.getItem('style');
     var title = style ? style : getPreferredStyleSheet();
     setActiveStyleSheet(title);
   }
 
   window.onunload = function(e) {
     var title = getActiveStyleSheet();
-    this.localStorage.setItem("style", title);
+    this.localStorage.setItem('style', title);
   }
 
-  var style = localStorage.getItem("style");
+  var style = localStorage.getItem('style');
   var title = style ? style : getPreferredStyleSheet();
   setActiveStyleSheet(title);
 })();

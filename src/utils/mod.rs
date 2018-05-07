10fa093eefbfd7bd50cd4962902437872b17327f
Anna <git@anna.lgbt>
2018-05-07 12:06:22 -0400

    refactor: use UUID wrapper types everywhere

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,5 @@
 pub mod hashed_password;
 pub mod recaptcha;
-pub mod simple_uuid;
 
 pub use self::hashed_password::HashedPassword;
 pub use self::recaptcha::ReCaptcha;
-pub use self::simple_uuid::SimpleUuid;

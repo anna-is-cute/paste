accecf51f77d87f25b1fef3b87fdbc4ef3634da3
Kyle Clemens <git@kyleclemens.com>
2018-05-09 08:45:26 -0400

    feat: implement login rate limiting

--- src/routes/web/auth/login.rs
+++ src/routes/web/auth/login.rs
@@ -5,6 +5,8 @@ use database::schema::users;
 use errors::*;
 use routes::web::{Rst, OptionalWebUser, Session};
 
+use chrono::{Utc, Duration, DateTime};
+
 use diesel::prelude::*;
 
 use rocket::State;
@@ -13,6 +15,10 @@ use rocket::request::Form;
 use rocket::response::Redirect;
 
 use rocket_contrib::Template;
+
+use std::collections::HashMap;
+use std::net::{SocketAddr, IpAddr};
+use std::sync::RwLock;
 
 #[get("/login")]
 fn get(config: State<Config>, user: OptionalWebUser, mut sess: Session) -> Rst {
@@ -36,8 +42,42 @@ struct RegistrationData {
   password: String,
 }
 
+// TODO: managed state or database instead?
+lazy_static! {
+  static ref ATTEMPTS: RwLock<HashMap<IpAddr, (DateTime<Utc>, usize)>> = Default::default();
+}
+
 #[post("/login", format = "application/x-www-form-urlencoded", data = "<data>")]
-fn post(data: Form<RegistrationData>, mut sess: Session, mut cookies: Cookies, conn: DbConn) -> Result<Redirect> {
+fn post(data: Form<RegistrationData>, mut sess: Session, mut cookies: Cookies, conn: DbConn, addr: SocketAddr) -> Result<Redirect> {
+  {
+    let mut attempts = ATTEMPTS.write().unwrap();
+    // get the record for this ip or set it to last request now and 0 attempts
+    let entry = attempts.entry(addr.ip()).or_insert_with(|| (Utc::now(), 0));
+
+    // increment the attempts
+    entry.1 += 1;
+
+    // if it's been 30 minutes since any request, clear the rate limiting
+    if Utc::now().signed_duration_since(entry.0) > Duration::minutes(30) {
+      *entry = (Utc::now(), 0);
+    }
+
+    if entry.1 < 5 {
+      // if rate limiting hasn't started yet, update the last request time so that we rate limit
+      // based on the attempt that started the limiting
+      entry.0 = Utc::now();
+    } else {
+      // otherwise, let them know that they'll need to wait
+      let msg = if entry.1 == 5 {
+        "Please try again in 30 minutes."
+      } else {
+        "Please try again later."
+      };
+      sess.data.insert("error".into(), msg.into());
+      return Ok(Redirect::to("/login"));
+    }
+  }
+
   let data = data.into_inner();
 
   let user: Option<User> = users::table

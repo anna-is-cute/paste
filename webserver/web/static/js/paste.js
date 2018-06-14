fa0fcdbc13aa1389d1d537458568717dbc297709
Kyle Clemens <git@kyleclemens.com>
2018-06-14 10:51:43 -0400

    feat: add option to view markdown source

--- webserver/web/static/js/paste.js
+++ webserver/web/static/js/paste.js
@@ -14,4 +14,32 @@
   for (var e of document.getElementsByClassName('closes-modal')) {
     e.onclick = closeModal;
   }
+
+  const tabs_container = document.getElementById('tabs-container');
+
+  if (tabs_container !== null) {
+    const rendered = document.getElementById('rendered-tab');
+    const rendered_a = rendered.firstChild;
+
+    const source = document.getElementById('source-tab');
+    const source_a = source.firstChild;
+
+    const rendered_content = tabs_container.querySelector('div#rendered-content');
+    const source_content = tabs_container.querySelector('div#source-content');
+
+    function swap(current, current_content, next, next_content) {
+      current.classList.remove('is-active');
+      next.classList.add('is-active');
+
+      current_content.classList.add('is-not-displayed');
+      next_content.classList.remove('is-not-displayed');
+    }
+
+    rendered_a.onclick = function() {
+      swap(source, source_content, rendered, rendered_content);
+    };
+    source_a.onclick = function() {
+      swap(rendered, rendered_content, source, source_content);
+    };
+  }
 })();

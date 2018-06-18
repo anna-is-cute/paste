9316ad3b2dafa943856b478c78ab3ae975be2cb7
Kyle Clemens <git@kyleclemens.com>
2018-06-17 23:31:11 -0400

    refactor: standardise certain JS choices

--- webserver/web/static/js/navbar.js
+++ webserver/web/static/js/navbar.js
@@ -1,20 +1,20 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
     // Get all "navbar-burger" elements
-    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
     // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
+    if (navbarBurgers.length > 0) {
       // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
+      navbarBurgers.forEach(function(el) {
+        el.addEventListener('click', function() {
           // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
+          const target_id = el.dataset.target;
+          const target = document.getElementById(target_id);
 
           // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
+          el.classList.toggle('is-active');
+          target.classList.toggle('is-active');
         });
       });
     }
--- webserver/web/static/js/notifications.js
+++ webserver/web/static/js/notifications.js
@@ -1,12 +1,12 @@
 (function() {
-  for (var button of document.querySelectorAll('.message > .message-header > .delete')) {
-    button.onclick = function() {
+  for (const button of document.querySelectorAll('.message > .message-header > .delete')) {
+    button.addEventListener('click', function() {
       this.parentElement.parentElement.remove();
-    };
+    });
   }
-  for (var button of document.querySelectorAll('.notification > .delete')) {
-    button.onclick = function() {
+  for (const button of document.querySelectorAll('.notification > .delete')) {
+    button.addEventListener('click', function() {
       this.parentElement.remove();
-    };
+    });
   }
 })();

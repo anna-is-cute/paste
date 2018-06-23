da77efb9891fd2249d35de5b7c73a1b1eb0acf93
Kyle Clemens <git@kyleclemens.com>
2018-06-23 14:51:10 -0400

    feat: make pre tags better in rendered markdown

--- webserver/web/static/js/highlight.js
+++ webserver/web/static/js/highlight.js
@@ -1,26 +1,40 @@
 (function() {
   for (const pre of document.getElementsByTagName('pre')) {
-    if (pre.id === '') {
-      continue;
-    }
-    const title = document.getElementById(pre.id + '-title');
-    if (title === null) {
-      continue;
-    }
-    const suffix = title.innerText.trim().split('.').pop();
-    const classes = [];
-    if (hljs.getLanguage(suffix) === undefined) {
-      classes.push('no-highlight');
-      classes.push('hljs');
-    } else {
-      classes.push('language-' + suffix);
-    }
-    for (const clazz of classes) {
-      pre.classList.add(clazz);
-    }
-    hljs.highlightBlock(pre);
-    hljs.lineNumbersBlock(pre, {
-      singleLine: true,
+    (function() {
+      if (pre.id === '') {
+        return;
+      }
+      const title = document.getElementById(pre.id + '-title');
+      if (title === null) {
+        return;
+      }
+      const suffix = title.innerText.trim().split('.').pop();
+      const classes = [];
+      if (hljs.getLanguage(suffix) === undefined) {
+        classes.push('no-highlight');
+        classes.push('hljs');
+      } else {
+        classes.push('language-' + suffix);
+      }
+      for (const clazz of classes) {
+        pre.classList.add(clazz);
+      }
+    })();
+
+    const cont = [...pre.classList].some(function(e) {
+      return e === 'hljs' || e.startsWith('language-');
     });
+
+    if (!cont) {
+      continue;
+    }
+
+    hljs.highlightBlock(pre);
+
+    if (pre.classList.contains('file-source')) {
+      hljs.lineNumbersBlock(pre, {
+        singleLine: true,
+      });
+    }
   }
 })();

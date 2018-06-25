b96e432c97b14424e49a40fae9bccfea24ba22ae
Kyle Clemens <git@kyleclemens.com>
2018-06-24 20:50:31 -0400

    fix: don't use onclick to swap theme

--- webserver/web/static/js/style.js
+++ webserver/web/static/js/style.js
@@ -1,51 +1,55 @@
-function setActiveStyleSheet(title) {
-  for (const a of document.getElementsByTagName('link')) {
-    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title')) {
-      a.disabled = true;
-      if (a.getAttribute('title') === title) {
-        a.disabled = false;
+(function() {
+  function setActiveStyleSheet(title) {
+    for (const a of document.getElementsByTagName('link')) {
+      if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title')) {
+        a.disabled = true;
+        if (a.getAttribute('title') === title) {
+          a.disabled = false;
+        }
       }
     }
+    localStorage.setItem('style', getActiveStyleSheet());
   }
-  localStorage.setItem('style', getActiveStyleSheet());
-}
 
-function getActiveStyleSheet() {
-  for (var a of document.getElementsByTagName('link')) {
-    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title') && !a.disabled) {
-      return a.getAttribute('title');
+  function getActiveStyleSheet() {
+    for (var a of document.getElementsByTagName('link')) {
+      if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('title') && !a.disabled) {
+        return a.getAttribute('title');
+      }
     }
+    return null;
   }
-  return null;
-}
 
-function getPreferredStyleSheet() {
-  for (var a of document.getElementsByTagName('link')) {
-    if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('rel').indexOf('alt') == -1 && a.getAttribute('title')) {
-      return a.getAttribute('title');
+  function getPreferredStyleSheet() {
+    for (var a of document.getElementsByTagName('link')) {
+      if (a.getAttribute('rel').indexOf('style') != -1 && a.getAttribute('rel').indexOf('alt') == -1 && a.getAttribute('title')) {
+        return a.getAttribute('title');
+      }
     }
+    return null;
   }
-  return null;
-}
 
-function swapTheme() {
-  var next;
-  if (getActiveStyleSheet() === 'dark') {
-    next = 'light';
-  } else {
-    next = 'dark';
+  function swapTheme() {
+    var next;
+    if (getActiveStyleSheet() === 'dark') {
+      next = 'light';
+    } else {
+      next = 'dark';
+    }
+    setActiveStyleSheet(next);
   }
-  setActiveStyleSheet(next);
-}
 
-(function() {
   function loadSheet() {
     const style = localStorage.getItem('style');
     const title = style ? style : getPreferredStyleSheet();
     setActiveStyleSheet(title);
   }
 
-  window.addEventListener('load', loadSheet);
+  window.addEventListener('load', function() {
+    loadSheet();
+
+    document.getElementById('swap_theme').addEventListener('click', swapTheme);
+  });
 
   window.addEventListener('unload', function() {
     const title = getActiveStyleSheet();

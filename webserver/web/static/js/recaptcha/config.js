600d02c5c8aaef054045616f77d0e5b2b8d880dd
Anna <git@anna.lgbt>
2018-07-13 14:27:35 -0400

    fix: update recaptcha
    [skip ci]

--- webserver/web/static/js/recaptcha/config.js
+++ webserver/web/static/js/recaptcha/config.js
@@ -2,5 +2,10 @@
   window['___grecaptcha_cfg'] = {
     'render': 'onload'
   };
+  window['grecaptcha'] = {
+    ready: function(f) {
+      (window['___grecatpcha_cfg']['fns'] = window['___grecaptcha_cfg']['fns'] || []).push(f);
+    },
+  };
   window['__google_recaptcha_client'] = true
 })();

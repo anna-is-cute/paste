abc92178d31608158fe11031d1aee9ebe73b7b92
Kyle Clemens <git@kyleclemens.com>
2018-07-13 21:59:53 -0400

    fix: correct notification deletion logic
    [skip ci]

--- webserver/web/src/js/notifications.js
+++ webserver/web/src/js/notifications.js
@@ -1,8 +1,8 @@
 (function() {
   for (const button of document.querySelectorAll('.message > .message-header > .delete')) {
-    button.addEventListener('click', () => this.parentElement.parentElement.remove());
+    button.addEventListener('click', e => e.currentTarget.parentElement.parentElement.remove());
   }
   for (const button of document.querySelectorAll('.notification > .delete')) {
-    button.addEventListener('click', () => this.parentElement.remove());
+    button.addEventListener('click', e => e.currentTarget.parentElement.remove());
   }
 })();

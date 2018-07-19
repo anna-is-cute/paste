37e587c1a10574df1cbf42592870b0128007a67d
Kyle Clemens <git@kyleclemens.com>
2018-07-18 21:31:45 -0400

    chore: actually push correct rust version

--- sidekiq.example.sh
+++ sidekiq.example.sh
@@ -12,13 +12,15 @@ RUST_ENV="release"
 CONF="sidekiq.yml"
 # The .env file path with SIDEKIQ_URL defined
 ENV_FILE=".env"
+# The path to the target directory (this should be ./.docker/run/target for docker)
+TARGET_DIR="./target"
 
 # Don't edit below here.
 
 source "$ENV_FILE"
 unset REDIS_URL
 
-LD_LIBRARY_PATH="./target/$RUST_ENV" \
+LD_LIBRARY_PATH="./$TARGET_DIR/$RUST_ENV" \
 REDIS_URL="$SIDEKIQ_URL" \
 sidekiq \
   -C "$CONF" \

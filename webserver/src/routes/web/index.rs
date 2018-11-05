e2a64b7b7861df96f59dad8e0d7ff9d5cac85c69
Anna <git@anna.lgbt>
2018-11-05 10:44:20 -0500

    feat: begin adding honeypots

--- webserver/src/routes/web/index.rs
+++ webserver/src/routes/web/index.rs
@@ -1,6 +1,6 @@
 use crate::{
   config::Config,
-  routes::web::{context, OptionalWebUser, Session},
+  routes::web::{context, AddCsp, Honeypot, OptionalWebUser, Session},
   utils::Language,
 };
 
@@ -11,8 +11,13 @@ use rocket_contrib::Template;
 use serde_json::json;
 
 #[get("/")]
-fn get(config: State<Config>, user: OptionalWebUser, mut sess: Session) -> Template {
+fn get(config: State<Config>, user: OptionalWebUser, mut sess: Session) -> AddCsp<Template> {
+  let honeypot = Honeypot::new();
   let mut ctx = context(&*config, user.as_ref(), &mut sess);
   ctx["languages"] = json!(Language::context());
-  Template::render("index", ctx)
+  ctx["honeypot"] = json!(honeypot);
+  AddCsp::new(
+    Template::render("index", ctx),
+    vec![format!("style-src '{}'", honeypot.integrity_hash)],
+  )
 }

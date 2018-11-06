9d2d757beceef9f003ae6aa51145ed2bd57bdb7a
Anna <git@anna.lgbt>
2018-11-06 13:12:04 -0500

    feat: disable the static files route in release versions

--- webserver/src/routes/web/static_files.rs
+++ webserver/src/routes/web/static_files.rs
@@ -2,9 +2,19 @@ use std::{io, path::PathBuf};
 
 use rocket::response::NamedFile;
 
+#[cfg(not(debug_assertions))]
+use rocket::response::status::BadRequest;
+
+#[cfg(debug_assertions)]
 #[get("/<path..>", rank = 1)]
 pub fn get(path: PathBuf) -> io::Result<NamedFile> {
   let static_path = PathBuf::from("webserver/web/static/");
   let resource_path = static_path.join(path);
   NamedFile::open(resource_path)
+}
+
+#[cfg(not(debug_assertions))]
+#[get("/<_path..>", rank = 1)]
+pub fn get(_path: PathBuf) -> BadRequest<&'static str> {
+  BadRequest(Some("The /static route is disabled in release versions. Please set up a reverse proxy and handle static files through it."))
 }

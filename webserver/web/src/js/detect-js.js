87c63652791fb184cfbcb6aa35f580184e62add5
Kyle Clemens <git@kyleclemens.com>
2018-11-12 16:58:01 -0500

    refactor: remove modernizr and replace it with a simpler file

---
+++ webserver/web/src/js/detect-js.js
@@ -0,0 +1,5 @@
+(function() {
+  const cl = document.children[0].classList;
+  cl.remove('no-js');
+  cl.add('js');
+})();
---
+++ webserver/web/static/js/detect-js.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+  var cl = document.children[0].classList;
+  cl.remove('no-js');
+  cl.add('js');
+})();
+//# sourceMappingURL=detect-js.js.map

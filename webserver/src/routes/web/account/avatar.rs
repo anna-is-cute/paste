db47cf71b9076aae82184b584ffb36e5273c8820
Anna <git@anna.lgbt>
2018-11-28 10:34:54 -0500

    feat: handle basic caching from gravatar

--- webserver/src/routes/web/account/avatar.rs
+++ webserver/src/routes/web/account/avatar.rs
@@ -30,10 +30,16 @@ thread_local! {
 pub enum Avatar<'r> {
   Avatar(Response<'r>),
   Status(Status),
+  #[response(status = 304)]
+  NotModified(()),
 }
 
 #[get("/account/avatar/<id>")]
-pub fn get<'r>(id: UserId, client: State<Client>, conn: DbConn) -> Result<Avatar<'r>> {
+pub fn get<'r>(id: UserId, client: State<Client>, if_mod: IfMod, conn: DbConn) -> Result<Avatar<'r>> {
+  const HEADERS: &[&str] = &[
+    "Content-Type", "Content-Length", "Cache-Control", "Expires", "Last-Modified",
+  ];
+
   let user: Option<User> = users::table.find(id).first(&*conn).optional()?;
   let user = match user {
     Some(u) => u,
@@ -50,21 +56,43 @@ pub fn get<'r>(id: UserId, client: State<Client>, conn: DbConn) -> Result<Avatar
   });
 
   let url = format!("https://gravatar.com/avatar/{}?s=128&d=identicon", hash);
-  let resp = client.get(&url).send()?;
+  let mut req = client.get(&url);
+  if let IfMod(Some(s)) = if_mod {
+    req = req.header("If-Modified-Since", s);
+  }
+  let resp = req.send()?;
+
+  if resp.status() == reqwest::StatusCode::NOT_MODIFIED {
+    return Ok(Avatar::NotModified(()));
+  }
 
   let mut builder = Response::build();
 
-  if let Some(h) = resp.headers().get("Content-Type") {
-    let h = Header::new("Content-Type", h.to_str()?.to_string());
-    builder.header(h);
+  for &name in HEADERS {
+    if let Some(h) = resp.headers().get(name) {
+      let h = Header::new(name, h.to_str()?.to_string());
+      builder.header(h);
+    }
   }
 
-  if let Some(h) = resp.headers().get("Content-Length") {
-    let h = Header::new("Content-Length", h.to_str()?.to_string());
-    builder.header(h);
-  }
+  let resp_status = resp.status();
 
-  builder.streamed_body(resp);
+  builder
+    .streamed_body(resp)
+    .raw_status(resp_status.as_u16(), resp_status.canonical_reason().unwrap_or(""));
 
   Ok(Avatar::Avatar(builder.finalize()))
+}
+
+pub struct IfMod(Option<String>);
+
+impl rocket::request::FromRequest<'a, 'r> for IfMod {
+  type Error = ();
+
+  fn from_request(req: &'a rocket::Request<'r>) -> rocket::request::Outcome<Self, Self::Error> {
+    match req.headers().get_one("If-Modified-Since") {
+      Some(ref m) => rocket::Outcome::Success(IfMod(Some(m.to_string()))),
+      None => rocket::Outcome::Success(IfMod(None)),
+    }
+  }
 }

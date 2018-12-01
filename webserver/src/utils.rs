7fe8820847b50af7a53db724f4adcb237676e402
Anna <git@anna.lgbt>
2018-12-01 18:06:02 -0500

    refactor: reorder utils modules

--- webserver/src/utils.rs
+++ webserver/src/utils.rs
@@ -1,11 +1,11 @@
 pub mod bit_mask;
 pub mod email;
-pub mod post_processing;
 pub mod form_date;
 pub mod hashed_password;
 pub mod language;
 pub mod multipart;
 pub mod password;
+pub mod post_processing;
 pub mod totp;
 pub mod validator;
 

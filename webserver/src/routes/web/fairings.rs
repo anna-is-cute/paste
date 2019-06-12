938e57241040df0defcb557e7327a03772df4c88
Anna <git@anna.lgbt>
2019-06-12 14:50:02 -0400

    fix: use one anti-csrf token per session
    Fixes #89.

--- webserver/src/routes/web/fairings.rs
+++ webserver/src/routes/web/fairings.rs
@@ -1,11 +1,9 @@
-pub mod anti_csrf;
 pub mod csp;
 pub mod last_page;
 pub mod push;
 pub mod security_headers;
 
 pub use self::{
-  anti_csrf::AntiCsrf,
   csp::Csp,
   last_page::LastPage,
   push::Push,

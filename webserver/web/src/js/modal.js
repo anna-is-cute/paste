a6508d375c4c2c678fc617749348752691ab7695
Kyle Clemens <git@kyleclemens.com>
2019-10-29 19:12:25 +0000

    feat: enable deleting anonymous pastes without JS

--- webserver/web/src/js/modal.js
+++ webserver/web/src/js/modal.js
@@ -1,4 +1,30 @@
 (function() {
+  function convertLinks() {
+    // the links will all be linked to a modal but have a href
+    for (const a of document.querySelectorAll('a[href][data-modal-id]')) {
+      // create a button
+      const button = document.createElement('button');
+      // copy all attributes but href
+      for (const attr of a.attributes) {
+        if (attr.name === 'href') {
+          continue;
+        }
+        button.setAttribute(attr.name, attr.value);
+      }
+      // fill the button with the link's children
+      for (const child of a.children) {
+        button.appendChild(child);
+      }
+      // make sure not to accidentally submit a form
+      button.setAttribute('type', 'button');
+      // replace the link
+      a.replaceWith(button);
+    }
+  }
+
+  // convert any links that should become modals if JS is enabled
+  convertLinks();
+
   function openModal(e) {
     document.getElementById(e.dataset.modalId).classList.add('is-active');
   }
--- webserver/web/static/js/modal.js
+++ webserver/web/static/js/modal.js
@@ -9,6 +9,96 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 (function () {
+  function convertLinks() {
+    // the links will all be linked to a modal but have a href
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = document.querySelectorAll('a[href][data-modal-id]')[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var a = _step.value;
+        // create a button
+        var button = document.createElement('button'); // copy all attributes but href
+
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
+
+        try {
+          for (var _iterator2 = a.attributes[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var attr = _step2.value;
+
+            if (attr.name === 'href') {
+              continue;
+            }
+
+            button.setAttribute(attr.name, attr.value);
+          } // fill the button with the link's children
+
+        } catch (err) {
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+              _iterator2.return();
+            }
+          } finally {
+            if (_didIteratorError2) {
+              throw _iteratorError2;
+            }
+          }
+        }
+
+        var _iteratorNormalCompletion3 = true;
+        var _didIteratorError3 = false;
+        var _iteratorError3 = undefined;
+
+        try {
+          for (var _iterator3 = a.children[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+            var child = _step3.value;
+            button.appendChild(child);
+          } // make sure not to accidentally submit a form
+
+        } catch (err) {
+          _didIteratorError3 = true;
+          _iteratorError3 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion3 && _iterator3.return != null) {
+              _iterator3.return();
+            }
+          } finally {
+            if (_didIteratorError3) {
+              throw _iteratorError3;
+            }
+          }
+        }
+
+        button.setAttribute('type', 'button'); // replace the link
+
+        a.replaceWith(button);
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return != null) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  } // convert any links that should become modals if JS is enabled
+
+
+  convertLinks();
+
   function openModal(e) {
     document.getElementById(e.dataset.modalId).classList.add('is-active');
   }
